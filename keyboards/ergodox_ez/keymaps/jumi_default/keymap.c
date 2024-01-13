abda746f272e3efbdba6d945b198fddc025c3f86
jumitaka <jun22sumi26+github@gmail.com>
2024-01-13 20:45:46 +0900

    Key fix.

--- keyboards/ergodox_ez/keymaps/jumi_default/keymap.c
+++ keyboards/ergodox_ez/keymaps/jumi_default/keymap.c
@@ -26,14 +26,12 @@ enum custom_keycodes {
     DEF_IME,
     W_IMEOFF,
     W_IMEON,
-    M_MCTL,
-    M_LPAD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_COMMON] = LAYOUT_ergodox_pretty(
     // left hand
-    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       MO(L_CONF),     _______,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
+    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       _______,        MO(L_CONF),    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,    _______,
     KC_TAB,     _______,    _______,    _______,    _______,    _______,    _______,        KC_UP,      _______,    _______,    _______,    _______,    _______,    _______,
     MO(L_FUNC), _______,    _______,    _______,    _______,    _______,                                _______,    _______,    _______,    _______,    _______,    _______,
     KC_LSFT,    _______,    _______,    _______,    _______,    _______,    KC_DEL,         KC_DOWN,    _______,    _______,    _______,    _______,    _______,    _______,
@@ -99,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_MAC_FN] = LAYOUT_ergodox_pretty(
     // left hand
     _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
-    _______,    KC_BRID,    KC_BRIU,    KC_MCTL,    KC_LPAD,     _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
+    _______,    KC_BRID,    KC_BRIU,    KC_MCTL,    KC_LPAD,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    KC_MPRV,    KC_MPLY,    _______,                                _______,    _______,    _______,    _______,    _______,    _______,
     _______,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    _______,    _______,                                                        _______,    _______,    _______,    _______,    _______,
@@ -123,26 +121,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [L_CONF] = LAYOUT_ergodox_pretty(
     // left hand
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,        OS_TGL,     _______,    _______,    _______,    _______,    _______,    QK_BOOT,
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    QK_BOOT,
     _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    _______,    _______,    _______,                                _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    _______,    _______,                                                        _______,    _______,    _______,    _______,    _______,
 
-                                                                _______,    _______,        _______,    _______,
-                                                                            _______,        _______,
-                                                    _______,    _______,    _______,        TO(L_NAV),  _______,    _______
+                                                                _______,    _______,        OS_TGL,     _______,
+                                                                            _______,        TO(L_QWERTY),
+                                                    _______,    _______,    _______,        TO(L_NAV),  _______,    TO(L_FUNC)
 )
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
-};
-
-void type_code(uint8_t keycode){
-    register_code (keycode);
-    unregister_code (keycode);
 };
 
 void toggle_default_layer(void){
@@ -173,21 +166,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code (KC_LALT);
             }
             return false;
-            break;
-        case M_MCTL:
-            if (record->event.pressed) {
-                host_consumer_send(0x29F);  // TODO:
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
-        case M_LPAD:
-            if (record->event.pressed) {
-                host_consumer_send(0x2A0);  // TODO:
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
             break;
         case W_IMEOFF:
             if (record->event.pressed) {
@@ -240,8 +218,7 @@ void matrix_init_user(void) {
     layer_move(L_QWERTY);
 };
 
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
+layer_state_t layer_state_set_user(layer_state_t state) {
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
@@ -259,23 +236,16 @@ void matrix_scan_user(void) {
             break;
     }
 
-    uint8_t layer = biton32(layer_state);
-
-    switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
-        case L_FUNC:
-            ergodox_right_led_2_on();
-            break;
-        case L_NAV:
-            ergodox_right_led_1_on();
-            ergodox_right_led_3_on();
-            break;
-        case L_CONF:
-            ergodox_led_all_on();
-            break;
-        default:
-            // none
-            break;
+    if (layer_state_cmp(state, L_FUNC)) {
+        ergodox_right_led_2_on();
+    }
+    if (layer_state_cmp(state, L_NAV)) {
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+    }
+    if (layer_state_cmp(state, L_CONF)) {
+        ergodox_led_all_on();
     }
 
-};
+    return state;
+}
--- keyboards/ergodox_ez/keymaps/jumi_default/rules.mk
+++ keyboards/ergodox_ez/keymaps/jumi_default/rules.mk
@@ -1,2 +1,3 @@
 #define DEBOUNCE 100
 DEBOUNCE_TYPE = sym_eager_pr
+# KEY_OVERRIDE_ENABLE = yes -- 使用可だが未使用
